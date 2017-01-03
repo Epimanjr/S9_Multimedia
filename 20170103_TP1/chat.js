6cbc58a496d35d60c65ab8fcadbf41654b570bd2
Antoine Nosal <antoine.nosal@gmail.com>
2017-01-03 16:04:47 +0100

    ajoute les yeux

--- 20170103_TP1/chat.js
+++ 20170103_TP1/chat.js
@@ -7,6 +7,14 @@ ctx.lineTo(40,10);
 ctx.lineTo(10,240);
 ctx.stroke();
 
+// Ajout des yeux
+ctx.beginPath();
+ctx.arc(74,94,26,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(126,94,26,0,2*Math.PI);
+ctx.stroke();
+
 // Ajout de la ligne rouge (étape 2)
 ctx.beginPath();
 ctx.moveTo(0,120);
