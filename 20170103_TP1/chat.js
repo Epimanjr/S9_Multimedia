7c572993964288dfdb2b93efe5bf91f6d2815f87
Maxime BLAISE <maxime.blaise12@gmail.com>
2017-01-03 15:54:00 +0100

    Etape 1/6

---
+++ 20170103_TP1/chat.js
@@ -0,0 +1,8 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(10,240);
+ctx.lineTo(190,240);
+ctx.lineTo(160,10);
+ctx.lineTo(40,10);
+ctx.lineTo(10,240);
+ctx.stroke();
