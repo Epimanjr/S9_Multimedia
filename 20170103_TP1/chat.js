7e41a2c5f321793481518b9fb7e7a03bcdbc4fd1
Maxime BLAISE <maxime.blaise12@gmail.com>
2017-01-03 16:00:11 +0100

    Etape 2 : en cours

--- 20170103_TP1/chat.js
+++ 20170103_TP1/chat.js
@@ -6,3 +6,10 @@ ctx.lineTo(160,10);
 ctx.lineTo(40,10);
 ctx.lineTo(10,240);
 ctx.stroke();
+
+// Ajout de la ligne rouge (étape 2)
+ctx.beginPath();
+ctx.moveTo(0,120);
+ctx.lineTo(200,120);
+ctx.strokeStyle = '#ff0000';
+ctx.stroke();
