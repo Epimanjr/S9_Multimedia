032f1682812c2017d07101c74311f13c488b2aec
Maxime BLAISE <maxime.blaise12@gmail.com>
2017-01-10 15:50:11 +0100

    App 2 fonctionnelle sur Firefox

--- 20170110_TP2/js/js_app2.js
+++ 20170110_TP2/js/js_app2.js
@@ -1,1 +1,15 @@
-console.log("bonjour");
+var audioElement = document.querySelector("video");
+var textTrack = audioElement.textTracks[0];
+textTrack.mode = 'hidden';
+console.log(textTrack);
+var data = document.getElementById("data");
+data.innerHTML = "Sous-titres ici !";
+textTrack.oncuechange = function (){
+    // "this" est un textTrack
+    var cue = this.activeCues[0]; // on suppose qu’on a 1 seul index.
+        if (!!cue) {
+            data.innerHTML = cue.startTime + "-" +
+            cue.endTime + ": " +
+            cue.text + "<br />";
+        }
+}
