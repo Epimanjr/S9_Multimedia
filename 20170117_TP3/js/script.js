2b00580ea14ac73dd5c78e43e8916adea43eecec
Maxime BLAISE <maxime.blaise12@gmail.com>
2017-01-17 15:57:33 +0100

    Amélioration de l'apprentissage

--- 20170117_TP3/js/script.js
+++ 20170117_TP3/js/script.js
@@ -2,6 +2,8 @@ $("#tableimage img").click(function () {
     // Récupération du texte à prononcer
     var text = this.id;
 
+    $("#imageactive").html("Tu as cliqué sur : " +text);
+
     // Speak!!
     var utterance = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(utterance);
