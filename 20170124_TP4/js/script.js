14ef367a02b1d5727d69363547dda585079b8584
Maxime BLAISE <maxime.blaise12@gmail.com>
2017-01-24 15:31:28 +0100

    Add btn snap + div image

--- 20170124_TP4/js/script.js
+++ 20170124_TP4/js/script.js
@@ -1,7 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$("#btn-snap").click(function() {
+   // Que faire quand on clique sur le bouton ? réponse ici : 
+   
+   
+   // Mise en place de l'image
+   $("#snapshot").html("bonjour");
+});
