433df661e8e52f7b17c3cf6ae928e80edcd1e8a2
Maxime BLAISE <maxime.blaise12@gmail.com>
2017-01-24 15:44:20 +0100

    Snapshot OK

--- 20170124_TP4/js/script.js
+++ 20170124_TP4/js/script.js
@@ -1,7 +1,13 @@
-$("#btn-snap").click(function() {
-   // Que faire quand on clique sur le bouton ? réponse ici : 
-   
-   
-   // Mise en place de l'image
-   $("#snapshot").html("bonjour");
+$("#btn-snap").click(function () {
+    // Que faire quand on clique sur le bouton ? réponse ici : 
+    
+    // 1°) Récupération de l'image 
+    var c = document.getElementById("inputCanvas");
+    var ctx = c.getContext("2d");
+    var imgData=ctx.getImageData(0,0,320,240);
+    
+    // 2°) Mise en place de l'image
+    var c2 = document.getElementById("outputCanvas");
+    var ctx2 = c2.getContext("2d");
+    ctx2.putImageData(imgData,0,0);
 });
